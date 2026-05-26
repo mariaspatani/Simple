d956f5a3eade83471a9e998a698e019d80fce0ee
Maria S Patani <mariaspatani2028@cs.sjcetpalai.ac.in>
2026-05-27 00:48:19 +0530

    Update Fcfs.c

--- Process Scheduling/Fcfs.c
+++ Process Scheduling/Fcfs.c
@@ -59,3 +59,64 @@ int main() {
     printf("\nAverage TAT = %.2f\n", totalTAT / n);
     return 0;
 }
+----------------------------------------------------------------------
+//without gantt chart
+    #include<stdio.h>
+int main(){
+  int n;
+  printf("Enter the number of processes:");
+  scanf("%d",&n);
+  int AT[n], BT[n], WT[n], TAT[n], CT[n], P[n];
+  for(int i=0;i<n;i++){
+    P[i]=i+1;
+    printf("Process P%d",i+1);
+    printf("Arrival Time ");
+    scanf("%d",&AT[i]);
+    printf("Burst Time ");
+    scanf("%d",&BT[i]);
+  }
+  //bubble sort
+  for(int i=0;i<n-1;i++){
+    for(int j=0;j<n-i-1;j++){
+       if(AT[j]>AT[j+1]){
+            int temp=AT[j];
+            AT[j]=AT[j+1];
+            AT[j+1]=temp;
+            
+            temp=BT[j];
+            BT[j]=BT[j+1];
+            BT[j+1]=temp;
+            
+            temp=P[j];
+            P[j]=P[j+1];
+            P[j+1]=temp;
+       }
+    }
+  }
+  int time=0;
+  float totalWT = 0, totalTAT = 0;
+
+  for(int i=0;i<n;i++){
+    if(time<AT[i])time=AT[i];
+    
+    time+=BT[i];
+    CT[i]=time;
+    TAT[i]=CT[i]-AT[i];
+    WT[i]=TAT[i]-BT[i];
+    
+    
+    totalWT+=WT[i];
+    totalTAT+=TAT[i];
+    
+  }
+  //output
+  printf("\n\nProcess\tAT\tBT\tCT\tTAT\tWT\n");
+  for(int i=0;i<n;i++){
+      printf("P%d\t%d\t%d\t%d\t%d\t%d\t\n",P[i],AT[i],BT[i],CT[i],TAT[i],WT[i]);
+  }
+  printf("\nAverage Waiting Time = %.2f", totalWT / n);
+  printf("\nAverage Turnaround Time = %.2f\n", totalTAT / n);
+
+    return 0;
+  
+}
